f4a211c7c0016537b60513a7c65eb6972b4bcbbf
Aadil Hafeez <aadil.hafeez@mycomera.com>
2024-01-29 13:00:12 +0400

    Handle remove observer crash

--- SDWebImageLinkPlugin/Classes/SDImageLinkLoader.m
+++ SDWebImageLinkPlugin/Classes/SDImageLinkLoader.m
@@ -135,7 +135,7 @@
     loaderContext.progressBlock = progressBlock;
     __block NSProgress *progress;
     progress = [imageProvider loadDataRepresentationForTypeIdentifier:(__bridge NSString *)kUTTypeImage completionHandler:^(NSData * _Nullable data, NSError * _Nullable error) {
-        if (progressBlock && progress) {
+        if (progressBlock && progress && [progress observationInfo]) {
             [progress removeObserver:self forKeyPath:NSStringFromSelector(@selector(fractionCompleted)) context:(__bridge void *)(loaderContext)];
         }
         if (operation.isCancelled) {
